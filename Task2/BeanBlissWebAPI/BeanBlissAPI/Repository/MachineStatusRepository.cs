a886a853d88c8fc4c4f4b50347ce66d493b1dbc1
Daryna Buravkova <164341933+NureBuravkovaDaryna@users.noreply.github.com>
2024-06-06 12:48:59 +0300

    Added Files

---
+++ Task2/BeanBlissWebAPI/BeanBlissAPI/Repository/MachineStatusRepository.cs
@@ -0,0 +1,49 @@
+﻿using BeanBlissAPI.Data;
+using BeanBlissAPI.Interfaces;
+using BeanBlissAPI.Models;
+
+namespace BeanBlissAPI.Repository
+{
+    public class MachineStatusRepository : IMachineStatusRepository
+    {
+        private readonly DataContext _context;
+
+        public MachineStatusRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CreateMachineStatus(MachineStatus machineStatus)
+        {
+            _context.Add(machineStatus);
+            return Save();
+        }
+
+        public MachineStatus GetMachineStatus(int machineId)
+        {
+            return _context.MachineStatus.Where(p => p.Id == machineId).FirstOrDefault();
+        }
+
+        public bool MachineCondition(int machineId)
+        {
+            var status = GetMachineStatus(machineId);
+            return status != null && status.EquipmentState;
+        }
+
+        public bool MachineStatusExists(int id)
+        {
+            return _context.MachineStatus.Any(p => p.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateMachineStatus(MachineStatus machineStatus)
+        {
+            _context.Update(machineStatus);
+            return Save();
+        }
+    }
+}
---
+++ Task2/BeanBlissWebAPI/BeanBlissAPI/Repository/TechnicianRepository.cs
@@ -0,0 +1,67 @@
+﻿using BeanBlissAPI.Data;
+using BeanBlissAPI.Interfaces;
+using BeanBlissAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeanBlissAPI.Repository
+{
+    public class TechnicianRepository : ITechnicianRepository
+    {
+        private readonly DataContext _context;
+
+        public TechnicianRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool DeleteTechnician(Technician Technician)
+        {
+            _context.Remove(Technician);
+            return Save();
+        }
+
+        public bool TechnicianExists(int TechnicianId)
+        {
+            return _context.Technician.Any(o => o.Id == TechnicianId);
+        }
+
+        public ICollection<Machine> GetMachinesByTechnician(int ownerId)
+        {
+            return _context.Machine.Where(p => p.Technician.Id == ownerId).ToList();
+        }
+
+        public Technician GetTechnician(int TechnicianId)
+        {
+            return _context.Technician.Where(o => o.Id == TechnicianId).FirstOrDefault();
+        }
+
+        public ICollection<Technician> GetTechnicians()
+        {
+            return _context.Technician.ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool UpdateTechnician(Technician Technician)
+        {
+            _context.Update(Technician);
+            return Save();
+        }
+
+        public bool CreateTechnician(Technician tech)
+        {
+            _context.Technician.Add(tech);
+            return Save();
+        }
+
+        public Technician GetTechnicianByMachineId(int machineId)
+        {
+            return _context.Machine.Include(m => m.Technician)
+            .Where(m => m.Id == machineId).Select(m => m.Technician)
+            .FirstOrDefault();
+        }
+    }
+}
